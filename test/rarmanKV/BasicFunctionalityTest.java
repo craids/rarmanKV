73b445666d0913f539a4410ab23e33c2b2b7dc63
Rebecca Weinberger <rew@mit.edu>
2018-11-26 19:32:42 -0500

    another test

--- test/rarmanKV/BasicFunctionalityTest.java
+++ test/rarmanKV/BasicFunctionalityTest.java
@@ -2,6 +2,8 @@ package rarmanKV;
 
 import org.junit.Before;
 import static org.junit.jupiter.api.Assertions.*;
+import java.security.SecureRandom;
+import java.util.*;
 import org.junit.jupiter.api.Test;
 import core.SimpleKV;
 
@@ -21,5 +23,23 @@ class BasicFunctionalityTest {
 		char[] actual = kv.read(key);
 		assertArrayEquals(val, actual);
 	}
-
+	
+	@Test
+	void testBigReadWrite() {
+		Set<char[]> written = new HashSet<>();
+		SecureRandom random = new SecureRandom();
+		
+		for (int i = 0; i < 100000; i++) {
+			byte[] keyBytes = new byte[16];
+            random.nextBytes(keyBytes);
+            char[] chars = new String(keyBytes).toCharArray();
+        	kv.write(chars, chars);
+        	written.add(chars);
+		}
+		
+		for (char[] chars : written) {
+			char[] actual = kv.read(chars);
+			assertArrayEquals(chars, actual);
+		}
+	}
 }
