c705b932f188ccf3ac13fcea55d5744fa95b7e23
Rayden Chia <rayden@craids-A1990.local>
2018-12-11 22:59:37 -0500

    test lab6

--- src/main/java/core/SimpleKV.java
+++ src/main/java/core/SimpleKV.java
@@ -1,74 +1,95 @@
 package core;
 
 import java.util.TreeMap;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.Iterator;
 
 public class SimpleKV implements KeyValue {
-	
-	private TreeMap<String,String> map;
-	
-    public SimpleKV() {
-    	this.map = new TreeMap<>();
-    }
 
-    @Override
-    public SimpleKV initAndMakeStore(String path) {
-    	return new SimpleKV();
-    }
+	private final String storeFile = "rkv.dat";
+	private TreeMap<String, String> map;
 
-    @Override
-    public void write(char[] key, char[] value) {
-    	map.put(new String(key), new String(value));
-    }
+	@SuppressWarnings("unchecked")
+	public SimpleKV() {
+		File mapstore = new File("rkv.dat");
+		if (mapstore.exists()) {
+			try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(mapstore))) {
+				map = (TreeMap<String, String>) ois.readObject();
+			} catch (Exception ex) {
+				this.map = new TreeMap<>();
+				return;
+			}
+		} else
+			this.map = new TreeMap<>();
+	}
 
-    @Override
-    public char[] read(char[] key) {
-		String skey = new String(key);
-		//if(map.containsKey(skey))
+	@Override
+	public SimpleKV initAndMakeStore(String path) {
+		return new SimpleKV();
+	}
+
+	@Override
+	public void write(char[] key, char[] value) {
+		map.put(new String(key), new String(value));
+	}
+
+	@Override
+	public char[] read(char[] key) {
 		return map.get(new String(key)).toCharArray();
-		//return null;
-    }
+	}
 
-    @Override
-    public Iterator<KVPair> readRange(char[] startKey, char[] endKey) {
+	@Override
+	public Iterator<KVPair> readRange(char[] startKey, char[] endKey) {
 		String start = new String(startKey);
 		String end = new String(endKey);
-		return new KVPairIterator(start,end);
-    }
-    
-    private class KVPairIterator implements Iterator<KVPair> {
-    	private Iterator<String> ksIterator;
-    	
-        public KVPairIterator(String start,String end)
-        {
-        	ksIterator = map.navigableKeySet().subSet(start, true, end, true).iterator();
-        }
-        
-        @Override
-        public boolean hasNext() {
-            return ksIterator.hasNext();
-        }
+		return new KVPairIterator(start, end);
+	}
 
-        @Override
-        public KVPair next() {
-            String nextKey = ksIterator.next();
-            return new KVPair(nextKey.toCharArray(), map.get(nextKey).toCharArray());
-        }
+	private class KVPairIterator implements Iterator<KVPair> {
+		private Iterator<String> ksIterator;
 
-        @Override
-        public void remove() {
-            throw new UnsupportedOperationException();
-        }
-    };
+		public KVPairIterator(String start, String end) {
+			ksIterator = map.navigableKeySet().subSet(start, true, end, true).iterator();
+		}
 
-    @Override
-    public void beginTx() {
-	System.out.println("Done!");
-    }
+		@Override
+		public boolean hasNext() {
+			return ksIterator.hasNext();
+		}
 
-    @Override
-    public void commit() {
-	System.out.println("Done!");
-    }
+		@Override
+		public KVPair next() {
+			String nextKey = ksIterator.next();
+			return new KVPair(nextKey.toCharArray(), map.get(nextKey).toCharArray());
+		}
+
+		@Override
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
+	};
+
+	@Override
+	public void beginTx() {
+		System.out.println("Done!");
+	}
+
+	@Override
+	public void commit() {
+		writeToStore();
+	}
+
+	public void writeToStore() {
+		try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(new File(storeFile)))) {
+			oos.writeObject(map);
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
 
 }
