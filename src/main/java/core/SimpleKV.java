b3cf14320b8ee74cf58d1765bae27e09526e5548
Rayden Chia <rayden@craids-A1990.local>
2018-12-12 16:22:03 -0500

    kvsockraddr

--- src/main/java/core/SimpleKV.java
+++ src/main/java/core/SimpleKV.java
@@ -2,8 +2,12 @@ package core;
 
 import rkv.*;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.SortedMap;
+import java.net.*;
+import java.nio.*;
+import java.nio.channels.SocketChannel;
 
 public class SimpleKV implements KeyValue {
 
@@ -20,6 +24,40 @@ public class SimpleKV implements KeyValue {
 				.closeOnExit()
 				.make();
 		map = db.createTreeMap(collectionName);
+		
+		try {
+		InetSocketAddress crunchifyAddr = new InetSocketAddress("18.40.21.160", 4433);
+		SocketChannel crunchifyClient = SocketChannel.open(crunchifyAddr);
+ 
+		//log("Connecting to Server on port 1111...");
+ 
+		ArrayList<String> companyDetails = new ArrayList<String>();
+ 
+		// create a ArrayList with companyName list
+		companyDetails.add("Facebook");
+		companyDetails.add("Twitter");
+		companyDetails.add("IBM");
+		companyDetails.add("Google");
+		companyDetails.add("CLOSE_CON");
+ 
+		for (String companyName : companyDetails) {
+ 
+			byte[] message = new String(companyName).getBytes();
+			ByteBuffer buffer = ByteBuffer.wrap(message);
+			crunchifyClient.write(buffer);
+ 
+			//log("sending: " + companyName);
+			buffer.clear();
+ 
+			// wait for 2 seconds before sending next message
+			Thread.sleep(2000);
+		}
+		crunchifyClient.close();
+		}
+		catch(Exception e)
+		{
+			
+		}
 	}
 	
 
