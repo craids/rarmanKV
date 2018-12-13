a1e96b86749e0afed1e4dc56b75a5c5c23a7b397
Rayden Chia <rayden@craids-A1990.local>
2018-12-12 22:57:54 -0500

    Update SimpleKV.java

--- src/main/java/core/SimpleKV.java
+++ src/main/java/core/SimpleKV.java
@@ -1,6 +1,7 @@
 package core;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.util.Iterator;
 
 public class SimpleKV implements KeyValue {
@@ -11,20 +12,21 @@ public class SimpleKV implements KeyValue {
 
 	@SuppressWarnings("unchecked")
 	public SimpleKV() {
-		File f = new File(storeName);
-		if (f.exists()) {
-			try {
-				FileInputStream fis = new FileInputStream(storeName);
-				ObjectInputStream ois = new ObjectInputStream(fis);
-				pt = (Trie<String>) ois.readObject();
-				ois.close();
-				fis.close();
-				return;
+		File dir = new File(".");
+		File [] files = dir.listFiles(new FilenameFilter() {
+		    @Override
+		    public boolean accept(File dir, String name) {
+		        return name.startsWith("rkv-");
+		    }
+		});
+		pt = new Trie<String>();
+		for (File f : files) {
+		    try {
+				pt.put(f.getName().substring(4), new String(Files.readAllBytes(f.toPath())));
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
-		pt = new Trie<String>();
 	}
 
 	@Override
@@ -35,7 +37,13 @@ public class SimpleKV implements KeyValue {
 	@Override
 	public void write(char[] key, char[] value) {
 		pt.put(new String(key), new String(value));
-		hasChanged = true;
+				try {
+					FileWriter fw = new FileWriter(new File("rkv-".concat(new String(key))));
+					fw.write(value);
+					fw.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 	}
 
 	@Override
@@ -55,17 +63,6 @@ public class SimpleKV implements KeyValue {
 
 	@Override
 	public void commit() {
-		if (hasChanged) {
-			try {
-				FileOutputStream fos = new FileOutputStream(storeName);
-				ObjectOutputStream oos = new ObjectOutputStream(fos);
-				oos.writeObject(pt);
-				oos.close();
-				fos.close();
-				hasChanged = false;
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
+
 	}
 }
