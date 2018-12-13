ae3ee381b376568fa2c8d0f442d43826158da4e9
Rayden Chia <rayden@craids-A1990.local>
2018-12-12 22:39:43 -0500

    Update SimpleKV.java

--- src/main/java/core/SimpleKV.java
+++ src/main/java/core/SimpleKV.java
@@ -7,6 +7,7 @@ public class SimpleKV implements KeyValue {
 
 	private Trie<String> pt;
 	private final String storeName = "rkv.dat";
+	private boolean hasChanged = false;
 
 	@SuppressWarnings("unchecked")
 	public SimpleKV() {
@@ -34,6 +35,7 @@ public class SimpleKV implements KeyValue {
 	@Override
 	public void write(char[] key, char[] value) {
 		pt.put(new String(key), new String(value));
+		hasChanged = true;
 	}
 
 	@Override
@@ -53,14 +55,17 @@ public class SimpleKV implements KeyValue {
 
 	@Override
 	public void commit() {
-		try {
-			FileOutputStream fos = new FileOutputStream(storeName);
-			ObjectOutputStream oos = new ObjectOutputStream(fos);
-			oos.writeObject(pt);
-			oos.close();
-			fos.close();
-		} catch (Exception e) {
-			e.printStackTrace();
+		if (hasChanged) {
+			try {
+				FileOutputStream fos = new FileOutputStream(storeName);
+				ObjectOutputStream oos = new ObjectOutputStream(fos);
+				oos.writeObject(pt);
+				oos.close();
+				fos.close();
+				hasChanged = false;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
